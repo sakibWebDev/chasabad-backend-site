0002cd5d5b03e1bce83783c85601de05d9fdeaac
sakibWebDev <sakibeazom2002@gmail.com>
2026-06-12 20:46:58 +0600

    my first dev

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
-import { envVars } from "./app/config/env";
+// import { envVars } from "./app/config/env";
 
 const app: Application = express();
 
@@ -13,8 +13,8 @@ app.use(cookieParser());
 // CORS
 app.use(cors({
     origin: [
-        envVars.FRONTEND_URL,
-        envVars.BETTER_AUTH_URL,
+        // envVars.FRONTEND_URL,
+        // envVars.BETTER_AUTH_URL,
         "http://localhost:3000",
         "http://localhost:5000"
     ],
